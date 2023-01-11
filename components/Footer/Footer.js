e7883158c69fa7346af01de9ea8ff6e8910d69c8
uddinmayraj <mayrajuddin03@gmail.com>
2023-01-11 11:35:58 +0600

    trying to create branch & collaborate

---
+++ components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='font-semibold text-red-500'>footeer by mayraj</p>
+        </div>
+    );
+};
+
+export default Footer;
