822346ba813bb5a6d04257dd202f4f08af8943e7
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-01-11 21:18:43 +0600

    git adder

--- components/MyGallery/GallerySection.js
+++ components/MyGallery/GallerySection.js
@@ -83,10 +83,10 @@ const GallerySection = () => {
             <h2 className='text-3xl font-bold'>OUR GALLERY</h2>
             <h4 className='text-lg font-xl'>Some of our project  discussion,management and meeting image</h4>
         </div>
-        <div className='grid mb-8 w-full lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2'> 
+        <div className='grid gap-6 row-gap-5 mb-8 w-full lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2'> 
             {Data.map(({id,title,desc,cover})=>
             <a key={id} href="/" aria-label="View Item">
-                <div className="relative overflow-hidden transition duration-200 transform shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+                <div className="relative overflow-hidden transition rounded duration-200 transform shadow-lg hover:-translate-y-2 hover:shadow-2xl">
                     <img src={cover} alt="" className='object-cover w-full h-56 md:h-64 xl:h-80'/>
                     <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
                         <p className='mb-4 text-lg font-bold text-gray-100'>{title}</p>
