3f0212ca2edb9b0159e3294a1507000a6b6f0c6c
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-01-11 13:43:35 +0600

    map section

--- components/MyGallery/GallerySection.js
+++ components/MyGallery/GallerySection.js
@@ -2,9 +2,32 @@ import React from 'react';
 
 const GallerySection = () => {
     return (
-        <div>
-            
+       <>
+         <div>
+            <div>
+                <h2>OUR GALLERY</h2>
+                <h4>Some of our project  discussion,management and meeting image</h4>
+            </div>
+            <div>
+                {Data.map((val)=>{
+                    return(   
+                <a>
+                    <div>
+                        <img src={val.cover} alt="" />
+                        <div>
+                            <p>{val.title}</p>
+                            <p>{val.desc}</p>
+                        </div>
+                        <div>
+                            <Fullscreen/>
+                        </div>
+                    </div>
+                </a>
+                    )
+                })}
+            </div>
         </div>
+       </>
     );
 };
 
