25a3210f182514d60a775c19b261686fb71a761d
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-01-11 13:00:09 +0600

    add gallery

---
+++ components/MyGallery/GalleryData.js
@@ -0,0 +1,74 @@
+const Data= [
+    {
+        id: 1,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+    {
+        id: 2,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+    {
+        id: 3,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+    {
+        id: 4,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+    {
+        id: 5,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+    {
+        id: 6,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+    {
+        id: 7,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+    {
+        id: 8,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+    {
+        id: 9,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+    {
+        id: 10,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+    {
+        id: 11,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+    {
+        id: 12,
+        title:"this is sport event management service",
+        desc:"it s event management service.that is responsible for managing events on our service",
+        cover:"",
+    },
+]
