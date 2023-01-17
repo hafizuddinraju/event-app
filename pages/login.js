401834af4c796ba11c3effe8cd1ce8a84c0623ca
uddinmayraj <mayrajuddin03@gmail.com>
2023-01-17 17:55:38 +0600

    emailJS implementation is done.

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+
+const Login = () => {
+    return (
+        <div className="mt-32">
+            <div className="container mx-auto">
+                <p className="text-center ">login</p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
