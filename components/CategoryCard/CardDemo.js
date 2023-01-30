e09e9ece5357ee07097f3cd1d863989600f895bc
mdansarulislam641 <mdansarulislam641@gmail.com>
2023-01-30 19:23:50 +0600

    list view category

--- components/CategoryCard/CardDemo.js
+++ components/CategoryCard/CardDemo.js
@@ -4,15 +4,15 @@ import { useState } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 
 import styles from '../../styles/CategoryCard.module.css';
-const CardDemo = ({ sub }) => {
-    console.log(sub);
+const CardDemo = ({ sub , gridView }) => {
+    // console.log(sub);
     const { name, image_url, description, _id } = sub
 
     const [hover, setHover] = useState(false);
     return (
         <div>
-            <div className={`${styles.relative} mx-auto`}>
-                <img className={styles.easing_imgg} src={image_url} alt="event" />
+            <div className={`${gridView ? "md:flex w-3/4 border-2 duration-300 border-indigo-300 rounded-xl" : styles.relative} mx-auto`}>
+                <img className={gridView ? "rounded-xl w-full md:h-[270px] lg:h-[224px] md:w-1/2 lg:w-3/4" :styles.easing_imgg} src={image_url} alt="event" />
                 <div className='hover:bg-pink-100 transition-all duration-1000'>
                     <div className="px-6 py-4">
                         <div className={styles.easing_title}>{name}</div>
--- pages/categories/index.js
+++ pages/categories/index.js
@@ -3,6 +3,7 @@ import Spinner from '../../components/Spinner/Spinner'
 import CardDemo from "../../components/CategoryCard/CardDemo";
 import CategoriesLayout from "../../layout/CategoriesLayout";
 import { getSubCategory } from "../../lib/helperSubCategory";
+import { useState } from "react";
 
 const AllCategoriesComponent = () => {
     const { data: subCate = [], error, isError, refetch, isLoading } = useQuery({
@@ -12,17 +13,21 @@ const AllCategoriesComponent = () => {
             return res;
         }
     })
+    const [gridView , setGridView] = useState(false);
     if (isLoading) return <Spinner></Spinner>
     return (
         <CategoriesLayout>
-            <div className="bg-[#F0F9FF]">
+          <div>
+          <button className={`hidden  z-50 fixed top-[110px] text-xl font-bold mb-10 md:flex md:justify-end border-2 border-indigo-500 px-2 py-2 rounded-md ${gridView ? "bg-blue-500 text-white" : null}`} onClick={()=>setGridView(!gridView)}>{gridView ? "Grid View" :"List View"}</button>
+          <div className="bg-[#F0F9FF]">
                 <h3 className="py-8 text-center text-5xl font-semibold">Events by Categories</h3>
-                <div className="grid grid-cols-1 md:grid-cols-2 ">
+                <div className={`md:mt-20 ${gridView ? "space-y-10" :"grid grid-cols-1 md:grid-cols-2"}`}>
                     {
-                        subCate?.map(sub => <CardDemo key={sub._id} sub={sub}></CardDemo>)
+                        subCate?.map(sub => <CardDemo gridView={gridView} key={sub._id} sub={sub}></CardDemo>)
                     }
                 </div>
             </div>
+          </div>
         </CategoriesLayout>
     );
 };
