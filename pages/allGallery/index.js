621c65e2147c79c47ff24723e2b403cf0dd88eb4
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-01-30 10:05:52 +0600

    add section

--- pages/allGallery/index.js
+++ pages/allGallery/index.js
@@ -27,7 +27,7 @@ const index = () => {
    <>
    <div className="">
         <h2 className="text-4xl mt-28 text-center font-semibold text-orange-600"><i>Our Event Gallery</i></h2>
-        <p className="text-center text-xl font-xl mt-3">Our event management all service are available,we provide this <br/> and our service Gallery</p>
+        <p className="text-center text-xl font-xl mt-3">Our event management all services are available,we provide this <br/> and our service Gallery</p>
         </div>
      <div className="grid gap-6 mb-8 my-24 max-w-screen-xl mx-auto lg:grid-cols-4 md:grid-cols-2 sm:row-gap-6 sm:grid-cols-1">
       {allPhotos.length > 0 &&
