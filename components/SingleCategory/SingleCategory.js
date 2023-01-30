400e6958282b5845463bdbfd21459552b805606f
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-01-30 10:03:27 +0600

    all gallery route

--- components/SingleCategory/SingleCategory.js
+++ components/SingleCategory/SingleCategory.js
@@ -1,53 +1,43 @@
-import { PhotoProvider, PhotoView } from 'react-photo-view';
-import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 import { MdOutlineReport, MdVerified } from "react-icons/md";
-import { useRouter } from 'next/router';
-import { createRef, useEffect, useState } from 'react';
-import Spinner from '../../components/Spinner/Spinner';
+import { useRouter } from "next/router";
+import { createRef, useEffect, useState } from "react";
+import Spinner from "../../components/Spinner/Spinner";
 
-import { useContext } from 'react';
+import { useContext } from "react";
 import Pdf from "react-to-pdf";
-import Modal from '../../components/Modal/Modal';
-import { AuthContext } from '../../context/AuthProvider';
-import { getEvent } from '../../lib/helperSubCategory';
-
-
-
-
+import Modal from "../../components/Modal/Modal";
+import { AuthContext } from "../../context/AuthProvider";
+import { getEvent } from "../../lib/helperSubCategory";
 
 const ref = createRef();
 const SingleCategory = () => {
-
-  const { user } = useContext(AuthContext)
-  const router = useRouter()
+  const { user } = useContext(AuthContext);
+  const router = useRouter();
   const [modal, setModal] = useState({});
-  const [eventData, setEventData] = useState({})
+  const [eventData, setEventData] = useState({});
   const id = router.query.eventId;
 
   useEffect(() => {
-
-    getEvent(id).then(res => {
-      console.log(res);
-      setEventData(res)
-
-    })
-      .catch(error => {
-        console.log(error)
+    getEvent(id)
+      .then((res) => {
+        console.log(res);
+        setEventData(res);
       })
-
-
-  }, [router.query.eventId])
-
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [router.query.eventId]);
 
   if (!eventData) {
-    return <Spinner></Spinner>
-
+    return <Spinner></Spinner>;
   }
 
   return (
     <>
-      <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 mx-10 md:mx-20 '>
-        <div ref={ref} className='col-span-2'>
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 mx-10 md:mx-20 ">
+        <div ref={ref} className="col-span-2">
           <div className="p-5 mx-auto sm:p-10 md:p-16 mt-20 text-gray-100">
             <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
               <PhotoProvider>
@@ -90,14 +80,27 @@ const SingleCategory = () => {
                     >
                       Price: {eventData?.price}
                     </div>
-                    <Pdf targetRef={ref} filename="Event_info.pdf" x={.5} y={.5} scale={0.8}>
-                      {({ toPdf }) =>
-                        <button onClick={toPdf} className='btn btn-sm md:py-0 my-3 text-black font-semibold bg-sky-400 hover:bg-sky-700'>Download PDF</button>}
-
+                    <Pdf
+                      targetRef={ref}
+                      filename="Event_info.pdf"
+                      x={0.5}
+                      y={0.5}
+                      scale={0.8}
+                    >
+                      {({ toPdf }) => (
+                        <button
+                          onClick={toPdf}
+                          className="btn btn-sm md:py-0 my-3 text-black font-semibold bg-sky-400 hover:bg-sky-700"
+                        >
+                          Download PDF
+                        </button>
+                      )}
                     </Pdf>
                   </div>
                 </div>
-                <div className="text-gray-100 text-justify">{eventData?.description}</div>
+                <div className="text-gray-100 text-justify">
+                  {eventData?.description}
+                </div>
                 <div className="flex justify-between items-center">
                   <div
                     rel="noopener noreferrer"
@@ -110,18 +113,15 @@ const SingleCategory = () => {
                     title="report product"
                     className="text-2xl text-red-600"
                   ></MdOutlineReport>
-
                 </div>
                 <div className="text-center">
-                  {eventData?.availability === '0' ? (
+                  {eventData?.availability === "0" ? (
                     <label className="inline-flex disabled:opacity-75 items-center justify-center w-1/2 h-10 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-gray-600 hover:bg-gray-700 focus:shadow-outline focus:outline-none">
                       Stock out
                     </label>
                   ) : (
                     <label
-                      htmlFor={
-                        user ? "booking-modal" : router.push('/login')
-                      }
+                      htmlFor={user ? "booking-modal" : router.push("/login")}
                       className="inline-flex items-center justify-center w-1/2 h-10 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-sky-500 hover:bg-sky-600 focus:shadow-outline focus:outline-none"
                       onClick={() => setModal(eventData)}
                     >
@@ -131,19 +131,17 @@ const SingleCategory = () => {
                 </div>
               </div>
             </div>
-            {modal && (
-              <Modal
-                modal={modal}
-                setModal={setModal}
-
-              ></Modal>
-            )}
+            {modal && <Modal modal={modal} setModal={setModal}></Modal>}
           </div>
         </div>
         <div>
           <div className="my-4 mx-auto mt-24 md:mt-36 lg:mt-36">
             <div>
-              <img className="mx-auto w-96 rounded-xl" src="https://i.ibb.co/yk0vDG1/Screenshot-2023-01-23-183803.png" alt="" />
+              <img
+                className="mx-auto w-96 rounded-xl"
+                src="https://i.ibb.co/yk0vDG1/Screenshot-2023-01-23-183803.png"
+                alt=""
+              />
             </div>
             <div className="my-4 text-center">
               <p className="font-bold text-lg">Open hour</p>
@@ -151,7 +149,7 @@ const SingleCategory = () => {
               <p className="text-sm">8.00am - 5.00pm</p>
               <p className="text-sm">Weekend closed</p>
             </div>
-            <div className='text-center'>
+            <div className="text-center">
               <p className="font-bold text-lg"> Phone&Email</p>
               <p className="text-sm">Phone: +084385922</p>
               <p className="text-sm">Email: event@bd.com</p>
@@ -162,7 +160,5 @@ const SingleCategory = () => {
     </>
   );
 };
-
-
 
 export default SingleCategory;
---
+++ components/galleryCard/GalleryCard.js
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import React from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+
+const GalleryCard = ({_id,cover,desc,title}) => {
+    return (
+       
+             <div
+              key={_id}
+              className="relative overflow-hidden transition rounded duration-200 transform shadow-lg hover: translate-y-2 hover:shadow-2xl"
+            >
+              <img
+                src={cover}
+                alt="pic"
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                width={100}
+                height={100}
+              />
+
+              <div className="absolute inset-0 px-6 py-4 top-0 left-0 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-4 text-lg font-bold text-gray-100">{title}</p>
+                <PhotoProvider>
+                  <PhotoView src={cover}>
+                    <img src={cover} alt="" className="w-full absolute top-0 left-0 opacity-0 h-full"
+                    width={100}
+                    height={100}
+                    />
+                  </PhotoView>
+                </PhotoProvider>
+
+                <p className="text-sm tracking-wide text-gray-300">{desc}</p>
+              </div>
+            </div>
+        
+    );
+};
+
+export default GalleryCard;
