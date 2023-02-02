4d2c9434a5467e45ba880111ab880dce4904de7b
uddinmayraj <mayrajuddin03@gmail.com>
2023-02-02 17:57:11 +0600

    change all category page card size grid btn

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -4,6 +4,7 @@ import CardDemo from "../../components/CategoryCard/CardDemo";
 import CategoriesLayout from "../../layout/CategoriesLayout";
 import { getSubCategory } from "../../lib/helperSubCategory";
 import { useState } from "react";
+import { FiGrid, FiList } from "react-icons/fi";
 
 const AllCategoriesComponent = () => {
     const { data: subCate = [], error, isError, refetch, isLoading } = useQuery({
@@ -13,21 +14,21 @@ const AllCategoriesComponent = () => {
             return res;
         }
     })
-    const [gridView , setGridView] = useState(false);
+    const [gridView, setGridView] = useState(false);
     if (isLoading) return <Spinner></Spinner>
     return (
         <CategoriesLayout>
-          <div>
-          <button className={`hidden  z-50 fixed top-[110px] text-xl font-bold mb-10 md:flex md:justify-end border-2 border-indigo-500 px-2 py-2 rounded-md ${gridView ? "bg-blue-500 text-white" : null}`} onClick={()=>setGridView(!gridView)}>{gridView ? "Grid View" :"List View"}</button>
-          <div className="bg-[#F0F9FF]">
-                <h3 className="py-8 text-center text-5xl font-semibold">Events by Categories</h3>
-                <div className={`md:mt-20 ${gridView ? "space-y-10" :"grid grid-cols-1 md:grid-cols-2"}`}>
-                    {
-                        subCate?.map(sub => <CardDemo gridView={gridView} key={sub._id} sub={sub}></CardDemo>)
-                    }
+            <div>
+                <button className={`hidden z-50 fixed right-20 top-[110px] text-xl font-bold mb-10 md:block  px-2 py-2 rounded-md ${gridView ? "bg-blue-500 text-white" : "border border-blue-500"}`} onClick={() => setGridView(!gridView)}>{gridView ? <FiGrid size={24} /> : <FiList size={24} />}</button>
+                <div className="">
+                    <h3 className="py-8 text-center text-5xl font-semibold">Events by Categories</h3>
+                    <div className={`md:mt-20 ${gridView ? "space-y-10" : "grid grid-cols-1 md:grid-cols-2 gap-4 w-4/5 mx-auto"}`}>
+                        {
+                            subCate?.map(sub => <CardDemo gridView={gridView} key={sub._id} sub={sub}></CardDemo>)
+                        }
+                    </div>
                 </div>
             </div>
-          </div>
         </CategoriesLayout>
     );
 };
--- pages/subcategory/[subcatId].js
+++ pages/subcategory/[subcatId].js
@@ -7,7 +7,7 @@ import CardDemo from '../../components/CategoryCard/CardDemo';
 import Spinner from '../../components/Spinner/Spinner';
 import CategoriesLayout from '../../layout/CategoriesLayout';
 import { getSingleSubCategory } from '../../lib/helperSubCategory';
-
+import { FiGrid, FiList } from "react-icons/fi";
 const SubCat = () => {
     const router = useRouter()
     const id = router.query.subcatId
@@ -28,19 +28,19 @@ const SubCat = () => {
 
 
     }, [router.query.subcatId])
-    console.log(data);
-    const [gridView , setGridView] = useState(false);
-    console.log(gridView);
+    // console.log(data);
+    const [gridView, setGridView] = useState(false);
+    // console.log(gridView);
     return (
         <CategoriesLayout>
-           <div className='relative'>
-            <button className={`hidden z-50 fixed top-[110px] text-xl font-bold mb-10 md:block border-2 border-indigo-500 px-2 py-2 rounded-md ${gridView ? "bg-blue-500 text-white" : null}`} onClick={()=>setGridView(!gridView)}>{gridView ? "Grid View" :"List View"}</button>
-           <div className={`md:mt-20 ${gridView ? "space-y-10" :"grid grid-cols-1 md:grid-cols-2 "}  `}>
-                {
-                    data?.map(sub => <CardDemo gridView={gridView} key={sub._id} sub={sub}></CardDemo>)
-                }
+            <div className='relative'>
+                <button className={`hidden z-50 fixed right-20 top-[110px] text-xl font-bold mb-10 md:block  px-2 py-2 rounded-md ${gridView ? "bg-blue-500 text-white" : "border border-blue-500"}`} onClick={() => setGridView(!gridView)}>{gridView ? <FiGrid size={24} /> : <FiList size={24} />}</button>
+                <div className={`md:mt-20 ${gridView ? "space-y-10" : "grid grid-cols-1 md:grid-cols-2 w-4/5 mx-auto gap-4"}  `}>
+                    {
+                        data?.map(sub => <CardDemo gridView={gridView} key={sub._id} sub={sub}></CardDemo>)
+                    }
+                </div>
             </div>
-           </div>
         </CategoriesLayout>
     );
 };
