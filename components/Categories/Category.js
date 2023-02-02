8e237424b299ea5d4e0bf061b36c5654f2370da2
mdansarulislam641 <mdansarulislam641@gmail.com>
2023-02-03 00:00:56 +0600

    main category card redesign

--- components/Categories/Category.js
+++ components/Categories/Category.js
@@ -19,7 +19,7 @@ const Category = () => {
     }
 
     return (
-        <div className=' max-w-screen-xl mx-auto'>
+        <div className='max-w-[1400px] mx-auto'>
             <div className='text-center my-20'>
                 <h3 className='font-bold text-3xl'>Events for You</h3>
             </div>
@@ -32,7 +32,7 @@ const Category = () => {
                     ></CategoryCard> )
                 }
             </div>
-            <div className='flex justify-center mb-20 ' >
+            <div className='flex justify-center mb-20 mt-10' >
                 <Link data-tip='Tap to explore more events' href='/categories' className='btn tooltip text-xl normal-case bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer border border-blue-500'>See more</Link>
             </div>
         </div>
