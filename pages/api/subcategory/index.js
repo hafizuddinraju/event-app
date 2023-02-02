4ffd221e65acab76f58a0614c53587d91fc11aee
hafizuddinraju <hafizraju.cse.bubt@gmail.com>
2023-02-03 01:43:31 +0600

    addevent page done

--- pages/api/subcategory/index.js
+++ pages/api/subcategory/index.js
@@ -1,20 +1,28 @@
-import { getSubCategory } from "../../../databaseConnection/controllerSubCategory";
+import {
+  getSubCategory,
+  postEvent,
+} from "../../../databaseConnection/controllerSubCategory";
 import connectMongo from "../../../databaseConnection/database";
 
 export default async function handler(req, res) {
-    connectMongo().catch(() => res.status(405).json({ error: "Error in the Connection" }))
+  connectMongo().catch(() =>
+    res.status(405).json({ error: "Error in the Connection" })
+  );
 
-    // type of request
-    const { method } = req
+  // type of request
+  const { method } = req;
 
-    switch (method) {
-        case 'GET':
-            // res.status(200).json({method,name:'Get'})
-            getSubCategory(req, res)
-            break;
-        default:
-            res.setHeader('Allow', ['GET']);
-            res.status(405).end(`Method ${method} Not Allow`)
-            break;
-    }
+  switch (method) {
+    case "GET":
+      // res.status(200).json({method,name:'Get'})
+      getSubCategory(req, res);
+      break;
+    case "POST":
+      postEvent(req, res);
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).end(`Method ${method} Not Allow`);
+      break;
+  }
 }
