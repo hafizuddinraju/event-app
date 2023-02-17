fed5119ad5ad8a011f0a8cf017715c41e51e8e2f
fatemakhatun7 <fatema.sprdnc@gmail.com>
2023-02-17 18:08:47 +0600

    faq blog changed

--- components/CategoryCard/CardDemo.js
+++ components/CategoryCard/CardDemo.js
@@ -11,29 +11,6 @@ const CardDemo = ({ sub, gridView }) => {
     const [hover, setHover] = useState(false);
     return (
         <div>
-            {/* <div className={`${gridView ? "md:flex w-3/4 border-2 duration-300 border-indigo-300 rounded-xl" : styles.demo} mx-auto`}>
-                <img className={gridView ? "rounded-xl w-full md:h-[270px] lg:h-[224px] md:w-1/2 lg:w-3/4" : styles.easing_imgg} src={image_url} alt="event" />
-                <div className='hover:bg-pink-100 transition-all duration-1000'>
-                    <div className="px-6 py-4">
-                        <div className={styles.easing_title}>{name}</div>
-                        <div className=' text-justify '>
-                            <p className={`${styles.easing_description} transition-all duration-1000 `}>
-                                {description.slice(0, 120)}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="px-6 py-4">
-                        <Link href={`/singleevent/${_id}`}>
-                            <button className={`${styles.easing_btn} transition-all duration-1000`}
-                                onMouseEnter={() => setHover(true)}
-                                onMouseLeave={() => setHover(false)}>
-                                Go <span className='inline-block align-middle'><FaArrowRight></FaArrowRight></span>
-                                {hover && <div className="absolute bottom-0 right-0 transition duration-1000 bg-white text-black p-2 rounded-md">Tap to see the details.</div>}
-                            </button>
-                        </Link>
-                    </div>
-                </div>
-            </div> */}
             <div className={`${gridView ? "md:flex w-3/4 border-2 duration-300 border-indigo-300 hover:border-indigo-400 rounded-xl ml-16 hover:ml-14" : "border-2 duration-300 border-indigo-300 rounded-xl hover:border-indigo-400"} mt-5 hover:mt-0 mx-auto`}>
             <img className={gridView ? "rounded-xl w-full md:h-[270px] lg:h-[245px] md:w-1/2 lg:w-2/4" : styles.easing_imgg} src={image_url} alt="event" />
             <div className='bg-white hover:bg-pink-100 transition-all duration-1000 rounded-xl'>
